ad75330fd0f1fb47888af3733d1f0fe9af170113
triste <zhenglfsir@gmail.com>
2018-05-17 11:37:18 +0800

    新增utils，eslint

---
+++ eslint-config-utils.js
@@ -0,0 +1,34 @@
+// https://eslint.org/docs/user-guide/configuring
+
+module.exports = {
+  root: true,
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  env: {
+    browser: true,
+    node: true
+  },
+  extends: ['airbnb-base'],
+  settings: {
+    flowtype: {
+      onlyFilesWithFlowAnnotation: false
+    }
+  },
+  // check if imports actually resolve
+  // add your custom rules here
+  globals: {
+    self: true
+  },
+  rules: {
+    semi: ['error', 'never'],
+    'no-param-reassign': [0],
+    'comma-dangle': ['error', 'never'],
+    // disallow reassignment of function parameters
+    // disallow parameter object manipulation except for specific exclusions
+    'no-param-reassign': ['off'],
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-console': [0]
+  }
+}
